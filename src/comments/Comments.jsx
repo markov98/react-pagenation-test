afc04942d62ad7fb7b4fa39e7f2d4665a8e82e8a
markov98 <74935630+markov98@users.noreply.github.com>
2024-05-14 15:07:39 +0300

    implamented simple pagination

--- src/comments/Comments.jsx
+++ src/comments/Comments.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
+
 export function Comments() {
-    const comments = [
+    const allComments = [
         { id: 1, username: "user1", comment: "This is comment 1." },
         { id: 2, username: "user2", comment: "This is comment 2." },
         { id: 3, username: "user3", comment: "This is comment 3." },
@@ -10,19 +12,32 @@ export function Comments() {
         { id: 8, username: "user8", comment: "This is comment 8." },
         { id: 9, username: "user9", comment: "This is comment 9." },
         { id: 10, username: "user10", comment: "This is comment 10." }
-      ];
+    ];
 
-  return (
-    <>
-    <h1>Comments</h1>
-    <ul>
-        {comments.map(comment => {
-            return <li key={comment.id}>
-                <p>{comment.username}</p>
-                <p>{comment.comment}</p>
-            </li>
-        })}
-    </ul>
-    </>
-  )
+    const [currPage, setCurrPage] = useState(1);
+    const commentsPerPage = 3;
+
+    const lastIndex = currPage * commentsPerPage;
+    const firstIndex = lastIndex - commentsPerPage;
+
+    const currComents = allComments.slice(firstIndex, lastIndex);
+
+    const nextPage = () => { setCurrPage(currPage + 1) }
+
+    return (
+        <>
+            <h1>Comments</h1>
+            <ul>
+                {currComents.map(comment => {
+                    return <li key={comment.id}>
+                        <p>{comment.username}</p>
+                        <p>{comment.comment}</p>
+                    </li>
+                })}
+            </ul>
+            <div className="pages">
+                <button onClick={nextPage}>Next Page</button>
+            </div>
+        </>
+    )
 }
