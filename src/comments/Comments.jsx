d8bd6462ca907c03af4ca7fcf3ed917b5e2b7b7d
markov98 <74935630+markov98@users.noreply.github.com>
2024-05-19 13:32:11 +0300

    moved comments to data file

--- src/comments/Comments.jsx
+++ src/comments/Comments.jsx
@@ -1,20 +1,8 @@
 import { useState } from "react";
+import { allComments } from "./data";
 import './Comments.css';
 
 export function Comments() {
-    const allComments = [
-        { id: 1, username: "user1", comment: "This is comment 1." },
-        { id: 2, username: "user2", comment: "This is comment 2." },
-        { id: 3, username: "user3", comment: "This is comment 3." },
-        { id: 4, username: "user4", comment: "This is comment 4." },
-        { id: 5, username: "user5", comment: "This is comment 5." },
-        { id: 6, username: "user6", comment: "This is comment 6." },
-        { id: 7, username: "user7", comment: "This is comment 7." },
-        { id: 8, username: "user8", comment: "This is comment 8." },
-        { id: 9, username: "user9", comment: "This is comment 9." },
-        { id: 10, username: "user10", comment: "This is comment 10." }
-    ];
-
     const [currPage, setCurrPage] = useState(1);
     const commentsPerPage = 3;
 
