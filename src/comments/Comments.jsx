3a368b27507ef104282532315eedcb40899a2dc2
markov98 <74935630+markov98@users.noreply.github.com>
2024-05-23 13:29:23 +0300

    updated pagination

--- src/comments/Comments.jsx
+++ src/comments/Comments.jsx
@@ -28,6 +28,7 @@ export function Comments() {
                 {currPage > 1 && <><Link to={`/${currPage - 1}`}>{currPage - 1}</Link> | </>}
                 <span>{currPage}</span>
                 {lastIndex < allComments.length && <> | <Link to={`/${currPage + 1}`}>{currPage + 1}</Link></>}
+                {lastIndex + commentsPerPage < allComments.length && <> | <Link to={`/${currPage + 2}`}>{currPage + 2}</Link></>}
             </div>
         </>
     )
